6b3c1625300241c014d6509f17c2ed3e242b9b89
2388MacUser <cesaretto.diego@hotmail.it>
2020-10-29 10:39:51 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+/* -------- Change Header BG on scroll -------- */
+$(function() {
+  $(window).on("scroll", function() {
+      if($(window).scrollTop() > 50) {
+          $("header").addClass("header__container--scrolled");
+      } else {
+         $("header").removeClass("header__container--scrolled");
+      }
+  });
+});
+
+
+
+
+/* -------- Open Panel Media Version -------- */
+$ (document).ready(function() {
+    $(".header__hamburger").on('click', function() {
+      $(".header__menu").toggleClass("header__menu--open");
+    })
+
+  });
+
+
+
